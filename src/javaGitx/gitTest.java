87010b5e9638847e002cf903b6323a3b226ed883
Anilkumar47-gamer <151371475+Anilkumar47-gamer@users.noreply.github.com>
2023-12-03 20:52:36 +0530

    gitrepoSave

--- src/javaGitx/gitTest.java
+++ src/javaGitx/gitTest.java
@@ -5,6 +5,9 @@ public class gitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello repo");
+git();
 	}
-
+public static void git() {
+	System.out.println("Hello This is git Repo");
+}
 }
